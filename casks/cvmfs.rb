87b6ae1819525b5f19baa66e17caa809f0696b9e
Valentin Volkl <valentin.volkl@cern.ch>
2024-12-03 15:52:58 +0100

    add 2.12

--- casks/cvmfs.rb
+++ casks/cvmfs.rb
@@ -1,17 +1,27 @@
 cask "cvmfs" do
-    version "2.11.5"
-    sha256 "204ede424c5e38563070b68a2034fb01400707e7cb98f28d05a71abc2143e9d7"
+    version "2.12.0"
   
-    url "https://ecsft.cern.ch/dist/cvmfs/cvmfs-#{version}/cvmfs-#{version}.pkg"
     name "CERN-VM FS macOS FUSE client"
     desc "Filesystem client for accessing scientific data using macOS FUSE"
     homepage "https://cernvm.cern.ch/fs/"
   
+    on_macos do
     # Declare dependency on another cask
     depends_on cask: "macos-fuse-t/homebrew-cask/fuse-t"
+
+    if Hardware::CPU.arm? 
+      url "https://ecsft.cern.ch/dist/cvmfs/cvmfs-#{version}/cvmfs-#{version}.fuse-t.arm64.pkg"
+      sha256 "8e28e5ffdc82dca9b0c88e6ff2d7ef285ca9a76b6e96966ff1c0c395b50784d8"
+      # Use the 'pkg' stanza for .pkg installation
+      pkg "cvmfs-#{version}.fuse-t.arm64.pkg"
+    end
+    if Hardware::CPU.intel?
+      url "https://ecsft.cern.ch/dist/cvmfs/cvmfs-#{version}/cvmfs-#{version}.fuse-t.intel.pkg"
+      sha256 "bc6dad1dd88aa38f84ebcc658c28e643ef05fe1a010559f0b24e57119d9cca30"
+      pkg "cvmfs-#{version}.fuse-t.intel.pkg"
+    end
+
   
-    # Use the 'pkg' stanza for .pkg installation
-    pkg "cvmfs-#{version}.pkg"
   
   end
   
